42d11da1f09b00a0df863f770949f92e2d49a1c2
JDagsa <jayden.dagsa@gmail.com>
2020-11-26 22:05:03 +0800

    edited version number ~ put to beta

--- gethash.sh
+++ gethash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-md5 ./debs/com.jaydendagsa.bigsurexpanded_1.0_iphoneos-arm.deb
-shasum -a 1 ./debs/com.jaydendagsa.bigsurexpanded_1.0_iphoneos-arm.deb
-shasum -a 256 ./debs/com.jaydendagsa.bigsurexpanded_1.0_iphoneos-arm.deb
+md5 ./debs/com.jaydendagsa.bigsurexpanded_0.1~Beta9_iphoneos-arm.deb
+shasum -a 1 ./debs/com.jaydendagsa.bigsurexpanded_0.1~Beta9_iphoneos-arm.deb
+shasum -a 256 ./debs/com.jaydendagsa.bigsurexpanded_0.1~Beta9_iphoneos-arm.deb
